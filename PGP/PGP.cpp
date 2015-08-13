9c436010c69d5e3b297ec374c06660852b6bae24
Munieshwar Ramdass <mramdass@nyu.edu>
2015-08-12 20:23:03 -0400

    Update and rename LAST.cpp to PGP.cpp

--- PGP/PGP.cpp
+++ PGP/PGP.cpp
@@ -1,6 +1,6 @@
 //	
 //	This tool can be used to achieve likelihood ratio for suspect(s) and/or victim(s) in a DNA mixture using probabilistic genotyping
-//	Legal Aid Statistical Tool Copyright (C) 2015 Munieshwar Ramdass, Nicholas J. Corpuz, Khagay Nagdimov
+//	Probabilistic Genotyping Program Copyright (C) 2015 Munieshwar Ramdass, Nicholas J. Corpuz, Khagay Nagdimov
 //	
 //	This program is free software: you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 //
 
 //
-//	Legal Aid Statistical Tool (LAST)
+//	Probabilistic Genotyping Program (PGP)
 //	
 //	Team:	Clinton Hughes		(Supervisor)
 //			Munieshwar Ramdass	(C++ Programmer)
